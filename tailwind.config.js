5811de86e4d2df7b6b78ae022197a3176aee8942
Dinton Harold Ainemukama <dintonharold@gmail.com>
2022-07-12 00:21:09 +0300

    Updated the package.json file with some scripts and uaadded the navbar to index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,33 @@
 module.exports = {
-  content: 
-  [
-     './public/index.html',
-    './src/**/*.{html,js}',
-  ],
+  content: ["./public/index.html", "./src/**/*.{html,js}", "./public/*.html"],
   theme: {
+    screens: {
+      sm: "480px",
+      // => @media (min-width: 480px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "976px",
+      // => @media (min-width: 976px) { ... }
+
+      xl: "1440px",
+      // => @media (min-width: 1440px) { ... }
+    },
+    colors: {
+      primary: "#005F33",
+      secondary: "#E6EFEA",
+      hoverPrimary: "#00BD65",
+      hoverSecondary: "#002413",
+      textColor: "#00331B",
+      textPrimaryColor: "#E6EFEA",
+      textSecondaryColor: "#00331B",
+      primaryDark: "#004927",
+      transparent: "#E6EFEA",
+
+      // ...
+    },
     extend: {},
   },
-  plugins: []
-}
+  plugins: [],
+};
